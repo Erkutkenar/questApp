d72a602f1df562b5b5d067b3c41290c6d09f98db
Erkut <lordkenar@gmail.com>
2024-04-21 00:41:43 +0300

    Interfaces and UserController firt step

--- src/main/java/com/project/questapp/entities/User.java
+++ src/main/java/com/project/questapp/entities/User.java
@@ -3,28 +3,21 @@ package com.project.questapp.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name="user")
 public class User {
 
     @Id
-    Long id;
+    private Long id;
 
-    String userName;
-    String password;
+    @Getter
+    @Setter
+    private String userName;
+    @Getter
+    @Setter
+    private String password;
 
-    public Object getUserName() {
-        return userName;
-    }
-
-    public Object getPassword() {
-        return password;
-    }
-
-    public void setUserName(Object userName) {
-    }
-
-    public void setPassword(Object password) {
-    }
 }
